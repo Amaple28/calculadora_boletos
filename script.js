cfb2c82ccaec295e97999c09bc528c863cc1b788
Maisa Gabriela <maisagabirodrigues@gmail.com>
2022-09-19 16:36:37 -0300

    funcionalidades dias da calculadora e quinta parcela.
    Co-authored-by: Lucas Eleutério Bueno <Crown32@users.noreply.github.com>

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ function calcular() {
     const parcelas = document.getElementById("parcelas").value;
     let valor = document.getElementById("valor-pedido").value;
     let data = document.getElementById("data-pedido").value;
+    let firstDays = document.getElementById("radio21").checked ? 21 : 28;
 
     const vtotal1 = document.getElementById("boleto1-valor");
     const dfinal1 = document.getElementById("boleto1-vencimento");
@@ -23,6 +24,9 @@ function calcular() {
     const vtotal4 = document.getElementById("boleto4-valor");
     const dfinal4 = document.getElementById("boleto4-vencimento");
 
+    const vtotal5 = document.getElementById("boleto5-valor");
+    const dfinal5 = document.getElementById("boleto5-vencimento");
+
     if (valor == '') {
         Swal.fire({
             icon: 'error',
@@ -33,48 +37,70 @@ function calcular() {
             icon: 'error',
             text: 'Favor preencher a data do pedido'
         })
-    } else if (parcelas == 1) {
+    }else if(!document.getElementById("radio21").checked && !document.getElementById("radio28").checked){
+        Swal.fire({
+            icon: 'error',
+            text: 'Favor selecionar os dias iniciais'
+        })
+    }else if (parcelas == 1) {
         vtotal1.innerHTML = `R$ ${parseFloat(valor)}`
-        dfinal1.innerHTML = addDays(data, 28).toLocaleDateString()
+        dfinal1.innerHTML = addDays(data, firstDays).toLocaleDateString()
     } else if (parcelas == 2) {
         valor = (valor / 2).toFixed(2)
 
         vtotal1.innerHTML = `R$ ${parseFloat(valor)}`
-        dfinal1.innerHTML = addDays(data, 28).toLocaleDateString()
+        dfinal1.innerHTML = addDays(data, firstDays).toLocaleDateString()
 
         vtotal2.innerHTML = `R$ ${parseFloat(valor)}`
-        dfinal2.innerHTML = addDays(data, 35).toLocaleDateString()
+        dfinal2.innerHTML = addDays(data, firstDays+7).toLocaleDateString()
     } else if (parcelas == 3) {
         valor = (valor / 3).toFixed(2)
 
         vtotal1.innerHTML = `R$ ${parseFloat(valor)}`
-        dfinal1.innerHTML = addDays(data, 28).toLocaleDateString()
+        dfinal1.innerHTML = addDays(data, firstDays).toLocaleDateString()
 
         vtotal2.innerHTML = `R$ ${parseFloat(valor)}`
-        dfinal2.innerHTML = addDays(data, 35).toLocaleDateString()
+        dfinal2.innerHTML = addDays(data, firstDays+7).toLocaleDateString()
 
         vtotal3.innerHTML = `R$ ${parseFloat(valor)}`
-        dfinal3.innerHTML = addDays(data, 42).toLocaleDateString()
+        dfinal3.innerHTML = addDays(data, firstDays+14).toLocaleDateString()
     } else if (parcelas == 4) {
         valor = (valor / 4).toFixed(2)
 
         vtotal1.innerHTML = `R$ ${parseFloat(valor)}`
-        dfinal1.innerHTML = addDays(data, 28).toLocaleDateString()
+        dfinal1.innerHTML = addDays(data, firstDays).toLocaleDateString()
 
         vtotal2.innerHTML = `R$ ${parseFloat(valor)}`
-        dfinal2.innerHTML = addDays(data, 35).toLocaleDateString()
+        dfinal2.innerHTML = addDays(data, firstDays+7).toLocaleDateString()
 
         vtotal3.innerHTML = `R$ ${parseFloat(valor)}`
-        dfinal3.innerHTML = addDays(data, 42).toLocaleDateString()
+        dfinal3.innerHTML = addDays(data, firstDays+14).toLocaleDateString()
 
         vtotal4.innerHTML = `R$ ${parseFloat(valor)}`
-        dfinal4.innerHTML = addDays(data, 56).toLocaleDateString()
+        dfinal4.innerHTML = addDays(data, firstDays+21).toLocaleDateString()
+    } else if (parcelas == 5) {
+        valor = (valor / 5).toFixed(2)
+
+        vtotal1.innerHTML = `R$ ${parseFloat(valor)}`
+        dfinal1.innerHTML = addDays(data, firstDays).toLocaleDateString()
+
+        vtotal2.innerHTML = `R$ ${parseFloat(valor)}`
+        dfinal2.innerHTML = addDays(data, firstDays+7).toLocaleDateString()
+
+        vtotal3.innerHTML = `R$ ${parseFloat(valor)}`
+        dfinal3.innerHTML = addDays(data, firstDays+14).toLocaleDateString()
+
+        vtotal4.innerHTML = `R$ ${parseFloat(valor)}`
+        dfinal4.innerHTML = addDays(data, firstDays+21).toLocaleDateString()
+
+        vtotal5.innerHTML = `R$ ${parseFloat(valor)}`
+        dfinal5.innerHTML = addDays(data, firstDays+28).toLocaleDateString()
     } else {
         Swal.fire({
             icon: 'error',
             text: 'Favor selecionar quantidade de parcelas.'
         })
-    }
+    } 
 
 }
 
@@ -91,6 +117,9 @@ function resetar() {
     const vtotal4 = document.getElementById("boleto4-valor");
     const dfinal4 = document.getElementById("boleto4-vencimento");
 
+    const vtotal5 = document.getElementById("boleto5-valor");
+    const dfinal5 = document.getElementById("boleto5-vencimento");
+
     vtotal1.innerHTML = '';
     dfinal1.innerHTML = '';
 
@@ -102,4 +131,7 @@ function resetar() {
 
     vtotal4.innerHTML = '';
     dfinal4.innerHTML = '';
+
+    vtotal5.innerHTML = '';
+    dfinal5.innerHTML = '';
 }
